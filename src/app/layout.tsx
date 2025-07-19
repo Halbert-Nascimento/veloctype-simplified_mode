1ee1f2f605a4ceb65d6ab16994da55181f226263
Halbert Nascimento <halbert.fac@gmail.com>
2025-07-19 12:07:27 -0300

    feat: adiciona arquivo ads.txt e corrige fechamento da tag script no layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -25,7 +25,7 @@ export default function RootLayout({
           async 
           src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-8858471184069465"
           crossOrigin="anonymous"
-        />
+        ></script>
       </head>
       <body className={inter.className}>
         <ThemeProvider>
